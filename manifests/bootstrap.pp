64c9037e53433e7caa1fe8e52ae9c3a0b3e7eb57
Peter Foley <peter@ifoley.id.au>
2014-05-10 23:55:21 +1000

    Setting up key sudo auth

--- manifests/bootstrap.pp
+++ manifests/bootstrap.pp
@@ -3,23 +3,34 @@ users { 'bootstrap': }
 
 class { 'apt' : }
 
-apt::source { 'ifoley.local' :
-    location      => 'http://i.ifoley.local/deb',
-      repos       => 'main',
-      include_src => false,
+package { 'libpam-ssh-agent-sudo':
+  ensure  => installed,
 }
+
+include sudo
+include sudo::configs
 
 # Source network information from hiera
 $net_config=hiera('network_config', {})
+create_resources('network_config', $net_config)
+
 
 # Source host information from hiera
 $host_information=hiera('host', {})
-
-$dotfiles_information=hiera('dotfiles', {})
+create_resources('host', $host_information)
 
 $dotfiles_repo_information=hiera('vcsrepo', {})
-
-create_resources('network_config', $net_config)
-create_resources('host', $host_information)
 create_resources('vcsrepo', $dotfiles_repo_information)
+
+$dotfiles_information=hiera('dotfiles', {})
 create_resources('dotfiles', $dotfiles_information)
+
+$pam_services_defaults = {
+  require => Package['libpam-ssh-agent-sudo'],
+}
+
+$pam_services=hiera('pam::services', {})
+create_resources('pam::service', $pam_services, $pam_services_defaults)
+
+$apt_repos=hiera('apt::sources', {})
+create_resources('apt::source', $apt::sources)
