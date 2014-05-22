2258d6decfb848db4774db948beee08249ad6b2e
Peter Foley <peter@ifoley.id.au>
2014-05-22 22:00:05 +1000

    Creating configuration files for sudo auth

--- manifests/bootstrap.pp
+++ manifests/bootstrap.pp
@@ -35,3 +35,20 @@ create_resources('dotfiles', $dotfiles_information)
 
 $apt_repos=hiera('apt::sources', {})
 create_resources('apt::source', $apt_repos)
+
+notify { 'Ensuring sudo authorized key file' : }
+file { '/etc/security/authorized_keys':
+  ensure  => present,
+  content => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA1ZpF3Yh5jN5aqYh/xbM0q4Ea2RVzH2PnSYRNj/z16wT6pi/QzKgWRy4KeSbIy+LBdlP+jssSL7GorcvLIOtPisJR3qIuuq/x90Z0SXRB8obZWwVJF6AOclUaqcDMMhnCN3HkDUHtQMyp5hglgVtWoSC+29LL2knWuxgG/P2kBk4lWvAHJqkOTnqkfsrHrgHfW+txUGkFTOQZkzKeVb9Hi0Eny+IA1i41AWuGtC8z6AzLJRCz9Av0vJZly+fUZVHren1alZUp6Rqhr9moherLovY7rX1xFExXeFFW45pFl1TkJeEgvNF19zr41w+EgdXBo7Ojqhy1wBnHGVgBxlzNZQ== root-root@bootstrap',
+  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+}
+
+file { '/etc/pam.d/sudo':
+  ensure  => present,
+  content => "#%PAM-1.0\nauth pam_ssh_agent_auth.so file=/etc/security/sudo_authorized_keys\n@include common-auth\n@include common-account\n@include common-session-noninteractive",
+  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+}
