7db720d726020843fa660a8614da7a796a921928
Peter Foley <peter@ifoley.id.au>
2014-06-07 14:43:13 +1000

    Update to use more hiera_include classes

--- manifests/bootstrap.pp
+++ manifests/bootstrap.pp
@@ -1,15 +1,10 @@
 
 users { 'bootstrap': }
 
-class { 'apt' : }
-
 package { 'libpam-ssh-agent-auth':
   ensure  => installed,
   require => Apt::Source['ifoley.local'],
 }
-
-include sudo
-include sudo::configs
 
 # Source network information from hiera
 $net_config=hiera('network_config', {})
@@ -26,17 +21,9 @@ create_resources('vcsrepo', $dotfiles_repo_information)
 $dotfiles_information=hiera('dotfiles', {})
 create_resources('dotfiles', $dotfiles_information)
 
-#$pam_services_defaults = {
-#  require => Package['libpam-ssh-agent-sudo'],
-#}
-
-#$pam_services=hiera('pam::services', {})
-#create_resources('pam::service', $pam_services, $pam_services_defaults)
-
 $apt_repos=hiera('apt::sources', {})
 create_resources('apt::source', $apt_repos)
 
-notify { 'Ensuring sudo authorized key file' : }
 file { '/etc/security/sudo_authorized_keys':
   ensure  => present,
   content => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA1ZpF3Yh5jN5aqYh/xbM0q4Ea2RVzH2PnSYRNj/z16wT6pi/QzKgWRy4KeSbIy+LBdlP+jssSL7GorcvLIOtPisJR3qIuuq/x90Z0SXRB8obZWwVJF6AOclUaqcDMMhnCN3HkDUHtQMyp5hglgVtWoSC+29LL2knWuxgG/P2kBk4lWvAHJqkOTnqkfsrHrgHfW+txUGkFTOQZkzKeVb9Hi0Eny+IA1i41AWuGtC8z6AzLJRCz9Av0vJZly+fUZVHren1alZUp6Rqhr9moherLovY7rX1xFExXeFFW45pFl1TkJeEgvNF19zr41w+EgdXBo7Ojqhy1wBnHGVgBxlzNZQ== root-root@bootstrap',
@@ -53,5 +40,4 @@ file { '/etc/pam.d/sudo':
   group   => 'root',
 }
 
-notify { "What is my role ${machine_role}" : }
-hiera_include('classes')
+hiera_include('classes', '')
