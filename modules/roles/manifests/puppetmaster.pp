c94f3d19453277360b92198831ae9a10d4745e4a
Peter Foley <peter@ifoley.id.au>
2014-06-12 22:01:53 +1000

    Fixed typos and missing command values

--- modules/roles/manifests/puppetmaster.pp
+++ modules/roles/manifests/puppetmaster.pp
@@ -3,12 +3,12 @@ class roles::puppetmaster {
   $my_ini_settings=hiera('ini_setting', {})
   create_resources('ini_setting', $my_ini_settings)
 
-  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
+  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ] }
 
   # Use r10k to download environments and hieradata
   exec { 'Download environments and hieradata':
-    command => 'r10k deploy environment -p',
-    notfiy  => Exec['Setup puppetmaster'],
+    command => 'r10k deploy environment -p -c /etc/r10k.yaml',
+    notify  => Exec['Setup puppetmaster'],
   } ->
   exec { 'Disable etckeeper':
     command     => 'mv /etc/apt/apt.conf.d/05etckeeper /root/tmp',
@@ -17,13 +17,13 @@ class roles::puppetmaster {
 
   # Need the second run due to etckeeper error during deb install
   exec { 'Setup puppetmaster':
-    command     => 'puppet -e "include profiles::puppetmaster"',
+    command     => 'puppet apply --config /etc/puppet/puppet.conf -e "include profiles::puppetmaster"',
     refreshonly => true,
     notify      => Exec['Enable etckeeper'],
   }
 
   exec { 'Enable etckeeper':
-    command     => 'mv /root/tmp /etc/apt/apt.conf.d/05etckeeper',
+    command     => 'mv /root/tmp/05etckeeper /etc/apt/apt.conf.d/05etckeeper',
     onlyif      => 'test -e /root/tmp/05etckeeper',
     refreshonly => true,
   }
